12bc16290ad243baf6d0c7f7e1599faa3d22ef73
Angel Marcel <angelmarcel364@gmail.com>
2024-12-14 22:39:58 +0300

    fourth commit

--- temperature.cpp
+++ temperature.cpp
@@ -6,3 +6,7 @@ void convertToFahrenheit(float celsius)
     float fahrenheit = (celsius * 9 / 5) + 32;
     cout << celsius << " Celsius is " << fahrenheit << " Fahrenheit." << endl;
 }
+
+void convertToCelsius(float fahrenheit)
+{
+    float celsius = (fahrenheit - 32) * 5 / 9;
