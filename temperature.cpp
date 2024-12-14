152f1a2f4a5a2dcb18bfb1c6f4b1bfba2cffcd4a
Angel Marcel <angelmarcel364@gmail.com>
2024-12-14 22:37:55 +0300

    third commit

--- temperature.cpp
+++ temperature.cpp
@@ -4,3 +4,5 @@ using namespace std;
 void convertToFahrenheit(float celsius)
 {
     float fahrenheit = (celsius * 9 / 5) + 32;
+    cout << celsius << " Celsius is " << fahrenheit << " Fahrenheit." << endl;
+}
